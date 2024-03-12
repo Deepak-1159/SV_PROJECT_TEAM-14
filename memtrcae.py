dd3462e5419bc457ab5fdac82abfd3acc93859c2
krishnapriyanarra <150409751+krishnapriyanarra@users.noreply.github.com>
2024-03-11 17:19:55 -0700

    Update memtrcae.py

--- memtrcae.py
+++ memtrcae.py
@@ -1,7 +1,7 @@
 import math
 
 file = open("trace.txt","w")
-for i in range(1,pow(2,20),1):
-	file.write(str(hex(i))+"\n")
+for i in range(1,pow(2,19),1):
+	file.write(str(hex(i%256))+"\n")
 
 file.close()
